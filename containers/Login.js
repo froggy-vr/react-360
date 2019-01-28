4e0bdff423d3100211d1973ba50e40e0cb1b8f4f
Nsamudera <nsamudera12@gmail.com>
2019-01-28 17:02:59 +0700

    added crawl

--- containers/Login.js
+++ containers/Login.js
@@ -7,6 +7,7 @@ import {
     StyleSheet,
     NativeModules,
     Image,
+    asset,
     Animated
 } from 'react-360';
 
@@ -24,21 +25,6 @@ export default class Login extends React.Component {
         userId: '',
         titleLocation: new Animated.Value(1),
         crawlLocation: new Animated.Value(0),
-        crawlText: `
-            In a Galaxy Far Far Away ... 
-            There lives a young Frog 
-            who has journey far and wide
-            in search for his destiny.
-
-            After much hardship and
-            years away from home,
-            the young Frog is ready
-            to go back.
-            
-            But one Challenge remained
-            before the Young Frog
-            can finally be at peace ...
-        `
     }
     componentDidMount() {
         this.titleAnimation()
@@ -47,24 +33,24 @@ export default class Login extends React.Component {
     titleAnimation() {
         this.state.titleLocation.setValue(0);
         Animated.spring(
-          this.state.titleLocation,
-          {
-            toValue: -0.2,
-            duration: 800,
-            friction: 2,
-            tension: 1 
-          }
+            this.state.titleLocation,
+            {
+                toValue: -0.2,
+                duration: 800,
+                friction: 2,
+                tension: 1
+            }
         ).start();
     }
     crawlAnimation() {
         this.state.crawlLocation.setValue(0);
         Animated.timing(
-          this.state.crawlLocation,
-          {
-            toValue: 20,
-            duration: 70000,
-            easing: Easing.linear
-          }
+            this.state.crawlLocation,
+            {
+                toValue: 20,
+                duration: 70000,
+                easing: Easing.linear
+            }
         ).start();
     }
     onClick = () => {
@@ -80,22 +66,28 @@ export default class Login extends React.Component {
                     if (snapshot.val()) {
                         this.props.startGame(this.state.userId)
                     }
-                  })
+                })
             })
         });
     }
 
     render() {
         const AnimatedText = Animated.createAnimatedComponent(Text);
-
         return (
             <View style={{ transform: [{ translateZ: -2 }, { translateY: 0.6 }, { translateX: -0.3 }] }}>
                 <AnimatedText
                     style={{
-                        transform: [{translateZ: 1}, { translateY: this.state.titleLocation }]
-                    }}    
-                > 
-                    Welcome To VROGGY 
+                        transform: [{ translateZ: 1 }, { translateY: this.state.titleLocation }],
+                    }}
+                >
+                    Welcome To VR
+                    <Image
+                        source={asset('frog.png')}
+                        style={{
+                            height: 1,
+                            width: 1,
+                        }}
+                    />GGY
                 </AnimatedText>
                 {this.state.userId !== '' ?
                     <View>
@@ -111,27 +103,37 @@ export default class Login extends React.Component {
                 {this.state.userId !== '' &&
                     <Image
                         source={{ uri: `https://chart.googleapis.com/chart?cht=qr&chl=${this.state.userId}&chs=256x256&choe=UTF-8&chld=L%7C2` }}
-                        style={{ 
-                            height: 0.5, 
-                            width: 0.5, 
+                        style={{
+                            height: 0.5,
+                            width: 0.5,
                             marginTop: 0.2,
-                            transform: [{translateX:1}, {translateY:-0.5}]
-                         }}
+                            transform: [{ translateX: 1 }, { translateY: 0.4 }]
+                        }}
                     />
                 }
-                <Animated.View
-                    style={{
-                        transform: [                        
-                            { translate: [-1.2, -2, -1] },
-                            {translateZ: 1}, 
-                            { translateY: this.state.crawlLocation }
-                        ]
-                    }}    
-                >
-                    <Text style={{color: 'yellow'}}> 
-                        {this.state.crawlText}
-                    </Text>
-                </Animated.View>
+                <Animated.Text style={{
+                    color: 'gold',
+                    transform: [                        
+                        { translate: [-1.2, -2, -1] },
+                        {translateZ: 1}, 
+                        { translateY: this.state.crawlLocation },
+                    ],
+                    zIndex: -100,
+                }} >
+                    In a Galaxy Far Far Away ...{"\n"}
+                    There lives a young Frog{"\n"}
+                    who has journey far and wide{"\n"}
+                    in search for his destiny.{"\n"}
+                    {"\n"}
+                    After much hardship and{"\n"}
+                    years away from home,{"\n"}
+                    the young Frog is ready{"\n"}
+                    to go back.{"\n"}
+                    {"\n"}
+                    But one Challenge remained{"\n"}
+                    before the Young Frog{"\n"}
+                    can finally be at peace ...
+                </Animated.Text>
             </View>
         );
     }
@@ -144,7 +146,11 @@ const styles = StyleSheet.create({
         borderColor: '#639dda',
         borderWidth: 0.01,
         borderRadius: 0.01,
+        width: 0.8
     },
+    crawl: {
+        color: "red"
+    }
 });
 
 AppRegistry.registerComponent('Login', () => Login);
